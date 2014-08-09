f5c5d49d7137a49bc64732faf703fb7c6a23c635
mgarvais <mgarvais@g2llc.com>
2014-08-09 11:51:55 -0700

    1st plot and histogram
    1st plot and histogram

---
+++ plot1.R
@@ -0,0 +1,26 @@
+## there are faster / better ways to load the data - but this will do for now
+
+# load up the file
+power<-read.table("household_power_consumption.txt", header=TRUE,sep=";")
+
+
+## Our target dates are 2007-02-01 and 2007-02-02
+## dates begin at 21998  last row is 24877
+
+target_dates <- subset(power, Date == '1/2/2007' | Date == '2/2/2007')
+
+#change date format
+target_dates$Date<-(as.Date(X$Date,format="%d/%m/%Y"))
+
+#set as numeric
+target_dates$Global_active_power<-as.numeric(as.character(target_dates$Global_active_power))
+
+
+##setup histogram 
+
+png(filename='plot1.png', width=480, height=480)
+
+hist(as.numeric(target_dates$Global_active_power), xlab='Global Active Power (Kilowatts)', main='Global Active Power', col='red')
+
+dev.off()
+
